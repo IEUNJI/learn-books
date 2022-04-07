68f4130dfb20371b9ec0b62844b1fc7d1e692d7a
林子峻 <linzijun1@jd.com>
2022-04-07 15:27:26 +0800

    泛型

--- TypeScript编程/src/函数.ts
+++ TypeScript编程/src/函数.ts
@@ -18,3 +18,59 @@ type Log2 = {
   (message: string, userId?: string): void;
   wasCalled: boolean;
 };
+
+/**
+ * 泛型
+ */
+
+// 调用函数时为T绑定具体类型
+type Filter1 = {
+  <T>(array: T[], f: (item: T) => boolean): T[];
+};
+
+// 使用类型时为T绑定具体类型
+type Filter2<T> = {
+  (array: T[], f: (item: T) => boolean): T[];
+};
+
+type Map1 = {
+  <T, U>(array: T[], f: (item: T) => U): U[];
+};
+
+type Map2<T, U> = {
+  (array: T[], f: (item: T) => U): U[];
+};
+
+/**
+ * 泛型约束
+ */
+type TreeNode = {
+  value: string
+};
+type LeafNode = TreeNode & {
+  isLeaf: true
+};
+type InnerNode = TreeNode & {
+  children: [TreeNode] | [TreeNode, TreeNode]
+};
+
+// T的上限为TreeNode
+function mapNode<T extends TreeNode>(
+  node: T,
+  f: (value: string) => string
+): T {
+  return {
+    ...node,
+    value: f(node.value)
+  };
+}
+
+/**
+ * call
+ */
+function call<T extends unknown[], R>(
+  f: (...args: T) => R,
+  ...args: T
+): R {
+  return f(...args);
+}
